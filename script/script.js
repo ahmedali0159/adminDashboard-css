53f17f04c0fa4cedb872e4a310629a6654f1aa06
ahmedali0159 <pirates0159@gmail.com>
2021-10-25 23:45:05 +0600

    added admin dashboard

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let sidebar = document.querySelector(".sidebar");
+let SidebarBtn = document.querySelector(".sidebarBtn");
+
+SidebarBtn.onclick = function(){
+   sidebar.classList.toggle("active"); 
+}
